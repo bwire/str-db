21670c78e73c2893301647a206ba361e5f8d03e1
Yevheniy Gandzyuck <evgeniy.gandzyuck@area9.dk>
2019-05-24 15:40:51 +0300

    First stage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class SwapiService {
+    _apiBase = 'https://swapi.co/api/';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    async getResource(url) {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Could not fetch ${url}! Received ${response.status}`);
+        }
+        return await response.json();
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    getAllPeople() {
+      return this.getResource(`${this._apiBase}people/`);
+    }
+
+    getAllPerson(id) {
+      return this.getResource(`${this._apiBase}people/:${id}`);
+    }
+}
