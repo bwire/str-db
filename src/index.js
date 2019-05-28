cb6e10d50faa022061cf6bfdc03b20ea923db3e1
Yevheniy Gandzyuck <evgeniy.gandzyuck@area9.dk>
2019-05-28 17:42:12 +0300

    Tue transfer

--- src/index.js
+++ src/index.js
@@ -1,42 +1,6 @@
-class SwapiService {
-    _apiBase = 'https://swapi.co/api/';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-    async getResource(url) {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Could not fetch ${url}! Received ${response.status}`);
-        }
-        return await response.json();
-    }
+import App from '../src/components/app'
 
-    async getAllPeople() {
-      const people = await this.getResource(`${this._apiBase}people/`);
-      return people.results;
-    }
-
-    getPerson(id) {
-      return this.getResource(`${this._apiBase}people/:${id}`);
-    }
-
-    async getAllPlanets() {
-      const planets = await this.getResource(`${this._apiBase}planets/`);
-      return planets.results;
-    }
-
-    getPlanet(id) {
-      return this.getResource(`${this._apiBase}planets/:${id}`);
-    }
-
-    async getAllStarships() {
-      const starships = await this.getResource(`${this._apiBase}starships/`);
-      return starships.results;
-    }
-
-    getStarship(id) {
-      return this.getResource(`${this._apiBase}starships/:${id}`);
-    }
-}
-
-new SwapiService().getAllPeople().then(elements => elements.forEach(element => {
-  console.log(element);
-}));
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/services/swapi-service.js
@@ -0,0 +1,42 @@
+export default class SwapiService {
+  _apiBase = 'https://swapi.co/api/';
+
+  async getResource(url) {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Could not fetch ${url}! Received ${response.status}`);
+      }
+      return await response.json();
+  }
+
+  async getAllPeople() {
+    const people = await this.getResource(`${this._apiBase}people/`);
+    return people.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`${this._apiBase}people/:${id}`);
+  }
+
+  async getAllPlanets() {
+    const planets = await this.getResource(`${this._apiBase}planets/`);
+    return planets.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`${this._apiBase}planets/:${id}`);
+  }
+
+  async getAllStarships() {
+    const starships = await this.getResource(`${this._apiBase}starships/`);
+    return starships.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`${this._apiBase}starships/:${id}`);
+  }
+}
+
+new SwapiService().getAllPeople().then(elements => elements.forEach(element => {
+console.log(element);
+}));
