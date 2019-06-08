f12f3fc99f9559e25417901b1445409d242adf52
BarbedWire <e.barbedwire@gmail.com>
2019-06-08 19:28:53 +0300

    componentWillUnmount

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -15,9 +15,13 @@ export default class RandomPlanet extends Component {
 
   componentDidMount = () => {  
     this.updatePlanet();  
-    setInterval(this.updatePlanet, 1500);
+    this.interval = setInterval(this.updatePlanet, 1500);
   }
   
+  componentWillUnmount = () => {
+    clearInterval(this.interval);
+  }
+
   onPlanetLoaded = (planet) => {
     this.setState({ 
       planet,
